9c95aaed30183040aadad6ef383739252fd41da8
Vangeyper <vangeyperr@gmail.com>
2023-03-15 20:40:28 +0100

    Inicio del proyecto

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+
+    <h1>Zombi Controler v1.0</h1>
+    <div class="Juego">
+      
+    </div>
+
+  </div>
+`;
+
+
